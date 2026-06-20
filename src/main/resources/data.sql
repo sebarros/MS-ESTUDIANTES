8b25ccb49bd8ad1b0cef196bb5014426febe6f9c
sebarros <alexbsebas061@gmail.com>
2026-06-20 15:53:25 -0400

    Replace Flyway with SQL initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO estudiante (rut, nombre, email, telefono, fecha_nacimiento, fecha_registro)
+VALUES ('12.345.678-9', 'Carlos Perez', 'carlosp@gmail.com', '912345678', '2000-05-10', CURRENT_DATE);
+
+INSERT INTO estudiante (rut, nombre, email, telefono, fecha_nacimiento, fecha_registro)
+VALUES ('98.765.432-1', 'Emilia Cardenas', 'emicardenas@gmail.com', '987654321', '1999-08-22', CURRENT_DATE);
+
+INSERT INTO estudiante (rut, nombre, email, telefono, fecha_nacimiento, fecha_registro)
+VALUES ('11.222.333-4', 'Camila Rojas', 'camila@gmail.com', '956789123', '2001-01-15', CURRENT_DATE);
